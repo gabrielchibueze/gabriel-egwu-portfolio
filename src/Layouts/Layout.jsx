059dffbb9859225a3c57e5003ffdd9703d67fa55
gabbyraw <gabrielchibueze@gmail.com>
2024-01-08 22:28:01 +0000

    updated version

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Footer from "./footer";
+import Footer from "./Footer";
 import Header from "./Header";
 import { Outlet } from 'react-router-dom'
 export default function (){
